6e4d7d2b5b787607e828498c905f2cd7a38b1744
Tinab <trandinhthien0104@gmail.com>
2024-09-11 16:36:59 +0700

    gui

--- Main/main.py
+++ Main/main.py
@@ -1,1 +1,6 @@
-print("huhuhu")
+from PyQt5 import QtCore, QtGui, QtWidgets
+import gui.app_ui
+
+gui.app_ui.process()
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BTL-Python
+- pip install pyqt5
+- pip install pyqt5-tools
